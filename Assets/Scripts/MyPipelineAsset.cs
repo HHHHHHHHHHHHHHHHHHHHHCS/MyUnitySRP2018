a89af52139fffce3d56696f1636599951e90edf7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-10-12 10:03:50 +0800

    temp

---
+++ Assets/Scripts/MyPipelineAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/MyPipeline", fileName = "MyPipelineAsset")]
+public class MyPipelineAsset : RenderPipelineAsset
+{
+    protected override IRenderPipeline InternalCreatePipeline()
+    {
+        return new MyPipeline();
+    }
+}
