968fb4b0f9e9699e383d81c17aee7adde260ca0e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-10-14 10:26:28 +0800

    add

--- Assets/Scripts/MyPipeline.cs
+++ Assets/Scripts/MyPipeline.cs
@@ -1,9 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
 
 public class MyPipeline : RenderPipeline
 {
+    private CullResults cull;
 
+    private readonly CommandBuffer cameraBuffer = new CommandBuffer()
+    {
+        name = "Render Camera"
+    };
+
+    public override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        base.Render(renderContext, cameras);
+        foreach (var camera in cameras)
+        {
+            Render(renderContext, camera);
+        }
+    }
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        if (!CullResults.GetCullingParameters(camera, out var cullingParameters))
+        {
+            return;
+        }
+
+        //CullResults cull = CullResults.Cull(ref cullingParameters, context);
+        CullResults.Cull(ref cullingParameters, context, ref cull);
+
+        context.SetupCameraProperties(camera);
+
+        cameraBuffer.BeginSample("Render Camera");
+
+        CameraClearFlags clearFlags = camera.clearFlags;
+        cameraBuffer.ClearRenderTarget((clearFlags & CameraClearFlags.Depth) != 0
+            , (clearFlags & CameraClearFlags.Color) != 0, Color.clear);
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
+
+
+        var drawSettings = new DrawRendererSettings(camera, new ShaderPassName("SRPDefaultUnlit"));
+        drawSettings.sorting.flags = SortFlags.CommonOpaque;
+        var filterSettings = new FilterRenderersSettings(true)
+        {
+            renderQueueRange = RenderQueueRange.opaque
+        };
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings);
+
+
+        context.DrawSkybox(camera);
+
+
+        drawSettings.sorting.flags = SortFlags.CommonTransparent;
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings);
+
+        cameraBuffer.EndSample("Render Camera");
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
+
+        context.Submit();
+    }
 }
