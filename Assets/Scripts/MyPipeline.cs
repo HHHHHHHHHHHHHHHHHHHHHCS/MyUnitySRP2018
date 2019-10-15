e0bf85f4cf080dc24862b052c14383aaec1013a3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-10-15 09:51:04 +0800

    add

--- Assets/Scripts/MyPipeline.cs
+++ Assets/Scripts/MyPipeline.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
 
 public class MyPipeline : RenderPipeline
 {
     private CullResults cull;
+    private Material errorMaterial;
+
 
     private readonly CommandBuffer cameraBuffer = new CommandBuffer()
     {
@@ -30,16 +33,25 @@ public class MyPipeline : RenderPipeline
             return;
         }
 
+#if UNITY_EDITOR
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            //将UI几何体发射到“场景”视图中以进行渲染。
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+
         //CullResults cull = CullResults.Cull(ref cullingParameters, context);
         CullResults.Cull(ref cullingParameters, context, ref cull);
 
         context.SetupCameraProperties(camera);
 
-        cameraBuffer.BeginSample("Render Camera");
 
         CameraClearFlags clearFlags = camera.clearFlags;
         cameraBuffer.ClearRenderTarget((clearFlags & CameraClearFlags.Depth) != 0
             , (clearFlags & CameraClearFlags.Color) != 0, Color.clear);
+
+        cameraBuffer.BeginSample("Render Camera");
         context.ExecuteCommandBuffer(cameraBuffer);
         cameraBuffer.Clear();
 
@@ -62,10 +74,39 @@ public class MyPipeline : RenderPipeline
         context.DrawRenderers(
             cull.visibleRenderers, ref drawSettings, filterSettings);
 
+        DrawDefaultPipeline(context, camera);
+
         cameraBuffer.EndSample("Render Camera");
         context.ExecuteCommandBuffer(cameraBuffer);
         cameraBuffer.Clear();
 
         context.Submit();
+    }
+
+    [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+    private void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera)
+    {
+        if (errorMaterial == null)
+        {
+            Shader errShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        var drawSettings = new DrawRendererSettings(camera, new ShaderPassName("ForwardBase"));
+
+        drawSettings.SetShaderPassName(1, new ShaderPassName("PrepassBase"));
+        drawSettings.SetShaderPassName(2, new ShaderPassName("Always"));
+        drawSettings.SetShaderPassName(3, new ShaderPassName("Vertex"));
+        drawSettings.SetShaderPassName(4, new ShaderPassName("VertexLMRGBM"));
+        drawSettings.SetShaderPassName(5, new ShaderPassName("VertexLM"));
+        drawSettings.SetOverrideMaterial(errorMaterial, 0);
+
+        var filterSettings = new FilterRenderersSettings(true);
+
+        context.DrawRenderers(
+            cull.visibleRenderers, ref drawSettings, filterSettings);
     }
 }
